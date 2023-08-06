8af62a441dd3b8ce32ba53a43d78112429a98cb7
leandjb <leandjb@gmail.com>
2023-08-06 16:59:41 -0500

    add button valitation unit test

--- examples/sync_test.py
+++ examples/sync_test.py
@@ -1,6 +1,15 @@
 import re
 from playwright.sync_api import Page, expect
 
-def test_sync_1(page: Page):
+def test_verify_title(page: Page):
     page.goto("https://playwright.dev/")
     expect(page).to_have_title(re.compile("Playwright")) #if "playwright" string is lowercase, FAILS
+
+def test_verify_button(page: Page):
+    page.goto("https://playwright.dev/")
+    expect(page).to_have_title(re.compile("apps"))
+
+    startedButton=page.locator("text=Get started")
+    expect(startedButton).to_have_attribute("href","/docs/intro")
+
+    startedButton.click()
